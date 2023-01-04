33d2afae5a3399a3e87a6836905d7eac83f924cf
BuguserOne <jinlingone@gmail.com>
2023-01-04 15:55:59 +0100

    first

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+console.log("start")
+
+
+//Multiplikation geht vor und so wird 5 * 10 zuerst durchgeführt.
+/* let score = 5 + 5 * 10;
+console.log('Ergebnis: ' + score); */
+
+//Du kannst Klammern() hinzufügen, um die Reihenfolge der Prioritäten zu ändern.
+/* let score = (5 + 5) * 10;
+console.log('Ergebnis: ' + score); */
+
+//Du kannst Werte auf Variablen addieren.
+/* let score = 0;
+score = score + 10;
+console.log('Ergebnis: ' + score); */
+
+//Hiervon gibt es eine verkürzte Version.
+/* let score = 0;
+score += 10;
+console.log('Ergebnis: ' + score); */
+
+//Inkrementierung und Dekrementierungen können wie folgt verwendet werden:
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+document.write(zahl);
+
+//Verwende Inkrement-Operatoren
+zahl++;
+console.log('increment: ' + zahl);
+
+//Verwende Dekrement-Operatoren
+zahl--;
+console.log('dekrement: ' + zahl);
+
